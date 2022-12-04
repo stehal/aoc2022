a2a8572b33cd8467bde7c810b62ac17d2ba3c393
Steven Hale <steven.hale@lm.se>
2022-12-04 08:23:52 +0100

    day04

--- day04/solution.py
+++ day04/solution.py
@@ -6,10 +6,11 @@ def two_sets(assignment):
     a1 = [int(n) for n in assignment[0].split("-")]
     a2 = [int(n) for n in assignment[1].split("-")]
     return set(range(a1[0], a1[1]+1)), set(range(a2[0], a2[1]+1))
+
+def solution(assignments, l):
+    return sum([l(*two_sets(a)) for a in assignments])
  
 
-overlaps = lambda r1, r2: r1.issubset(r2) or (r2).issubset(r1)
-print(f'Solution 1: {sum([overlaps(*two_sets(a)) for a in assignments])}')
+print(f'Solution 1: {solution(assignments, lambda r1, r2: r1.issubset(r2) or (r2).issubset(r1))}')
 
-contains = lambda r1, r2: len(set(r1) & set(r2)) > 0
-print(f'Solution 2: {sum([contains(*two_sets(a)) for a in assignments])}')
+print(f'Solution 2: {solution(assignments, lambda r1, r2: len(set(r1) & set(r2)) > 0)}')
